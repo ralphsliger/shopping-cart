d2d1727e337c843a46c4424973e49d20c5d6913c
nymeria <ralphsliger1@gmail.com>
2022-01-15 16:17:34 -0500

    part 2

--- shopping-cart/src/components/Phone.js
+++ shopping-cart/src/components/Phone.js
@@ -1,5 +1,35 @@
 import React, { Component } from 'react'
+import React, { Component } from 'react'
+import { DragSource } from 'react-dnd';import { ItemTypes } from './Constants';
 
+
+let collect = (connect, monitor) => {
+    return {
+        connectDragSource: connect.dragSource(),
+        isDragging: monitor.isDragging()
+    }
+}
+
+// phone DnD spec
+const phoneSpec = {
+    beginDrag(props) {
+        console.log("begin drag")
+        return {
+            name: props.brand
+
+        }
+    },
+    endDrag(props, monitor, component) {
+        if (monitor.didDrop()) {
+            const dragItem = monitor.getItem(); // from beginDrag
+            const dropResult = monitor.getDropResult();
+            // Move action goes here
+            console.log("You dropped ", dragItem.name, ' into ' + dropResult.name)
+        } else {
+            return;
+        }
+    }
+}
 class Phone extends Component{
     render()
     {
