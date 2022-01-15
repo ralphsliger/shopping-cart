82438bdde7dfbc36925c5c06b491e0209839e9a5
nymeria <ralphsliger1@gmail.com>
2022-01-15 14:23:55 -0500

    Part 1

---
+++ shopping-cart/src/components/Phone.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Phone extends Component{
+    render()
+    {
+    const { name } = this.props;
+    return(
+        <div className='phone'>
+            { name }
+        </div>
+        )
+    }
+    }export default Phone
